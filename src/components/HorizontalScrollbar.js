7b145a373a2a39cef7597a9abb4d4b867d8f1e64
ra8200 <razi.alawamleh@gmail.com>
2023-04-12 17:43:08 -0400

    "second commit"

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import { Box } from '@mui/material'
+import React, { useContext} from 'react'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { Box, Typography } from '@mui/material';
 
-const HorizontalScrollbar = ({ data }) => {
+import ExerciseCard from './ExerciseCard';
+import BodyPart from './BodyPart';
+
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
   return (
-    <div>
-        Hello
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
         {data.map((item) => (
                 <Box
                     key={item.id || item}
@@ -12,11 +38,13 @@ const HorizontalScrollbar = ({ data }) => {
                     title={item.id || item}
                     m="0 40px"
                 >
-                    Hello
+                    {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+                    : <ExerciseCard exercise={item} />  
+                    }
                 </Box>
             )
         )}
-    </div>
+    </ScrollMenu>
   )
 }
 
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,14 +4,13 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     const [search, setSearch] = useState('')
-    const [exercises, setExercises] = useState([]);
     const [bodyParts, setBodyParts] = useState([])
     
     useEffect(() => {
         const fetchExercisesData = async () => {
-            const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyparts', exerciseOptions);
+            const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
 
             setBodyParts(['all', ...bodyPartsData]);
         }
@@ -76,7 +75,7 @@ const SearchExercises = () => {
             </Button>
         </Box>
         <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
-                <HorizontalScrollbar data={bodyParts}/>
+                <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} isBodyParts />
         </Box>
     </Stack>
   )
